7f2fbc4a301cbdfa09486c3cbea3e5a2c0ea8c31
1-2-1-2 <v.sangwan9876@gmail.com>
2020-07-29 12:47:06 +0530

    end of chapterone

---
+++ 1.first.c
@@ -0,0 +1,26 @@
+# include<stdio.h>
+
+int main(){
+	printf("Hello this is first c programme");
+	return 0;
+}
+
+
+/* 
+
+There are 32 reserver Keywords in C language which you
+restricted to use.
+
+Instructions are executed in sequential order.*
+
+
+Basic operation of C file:-
+
+first.c ---> compiler----> the system run this file.
+
+Complier - (Changes to .exe or any other format)  
+Also it is changes to default format acc to operating system 
+for windows it is .exe or .out for linux
+
+*/
+
---
+++ 4.practice_01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+	float celsius = 37, far;
+	far = (celsius *9 / 5) + 32;
+
+	printf(" The value of this celsius in fahrenheit is %f\n", far);
+	return 0;
+}
