d84fb526c0df0ad39e4639d390abc09d1b36461b
1-2-1-2 <v.sangwan9876@gmail.com>
2020-07-30 22:56:42 +0530

    This is 2nd commit

---
+++ 02.Instructions-operators/01.type_declaration_instruction.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+	//Type declaration instruction
+	
+	int a = 4; //Type declaration instruction
+	int b = 5,c=0,d; //Type declaration instruction
+
+	printf(" The value of a is %d\n",a);
+	printf(" The value of b is %d\n",b);
+	printf(" The value of c is %d\n",c);
+	printf(" The value of d is %d\n",d);
+	
+
+	float x = 1.2;
+	float y = 1 + 8.8;
+
+	printf(" The value of x is %f\n",x);
+
+	return 0;
+}
---
+++ 02.Instructions-operators/02.arithmetic_instruction.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+
+	int a =4;
+	int b =8;
+
+	printf("The value of a + b %d\n",a+b);
+	printf("The value of a - b %d\n",a-b);
+	printf("The value of a * b %d\n",a*b);
+	printf("The value of a / b %d\n",a/b);
+}
