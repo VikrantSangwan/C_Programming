80169c0e624e4debe547591abb199e5a1459bdfc
1-2-1-2 <v.sangwan9876@gmail.com>
2020-07-30 23:01:02 +0530

    Commenting commit

--- 02.Instructions-operators/01.type_declaration_instruction.c
+++ 02.Instructions-operators/01.type_declaration_instruction.c
@@ -3,7 +3,7 @@
 int main(){
 
 	//Type declaration instruction
-	
+
 	int a = 4; //Type declaration instruction
 	int b = 5,c=0,d; //Type declaration instruction
 
@@ -12,6 +12,8 @@ int main(){
 	printf(" The value of c is %d\n",c);
 	printf(" The value of d is %d\n",d);
 	
+
+	//Commenting float values..........
 
 	float x = 1.2;
 	float y = 1 + 8.8;
